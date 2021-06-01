2ee06b738d865fa70ee52275700d66ebe8ba0450
Milica <djoric.milica91@yahoo.com>
2021-06-01 16:07:32 -0400

    day 7

--- src/test/java/com/cybertek/tests/day7_alerts_iframes_windows/AlertsPractice.java
+++ src/test/java/com/cybertek/tests/day7_alerts_iframes_windows/AlertsPractice.java
@@ -1,13 +1,17 @@
 package com.cybertek.tests.day7_alerts_iframes_windows;
 
 import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
 public class AlertsPractice {
+
     WebDriver driver;
 
     @BeforeMethod
@@ -18,7 +22,16 @@ public class AlertsPractice {
         driver.get("http://practice.cybertekschool.com/javascript alerts");
     }
     @Test
-    public void information_alart_test(){
+    public void information_alert_test(){
+        //Locating the button for alert to click
+        WebElement informationAlertButton = driver.findElement(By.xpath("//button[.='Click for JS Alert']"));
+        //3. Click to "Click for JS Alert" buton
+        informationAlertButton.click();
+        //4. Click to OK button from the alert
+        Alert alert = driver.switchTo().alert();
+        //5. Verify "You sucessfuly clicked an alert" text is displayed
+
+
 
     }
 }

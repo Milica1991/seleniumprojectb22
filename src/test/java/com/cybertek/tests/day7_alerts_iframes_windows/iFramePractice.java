f7ad2f13cec622a62eaab95d292881fb9607e625
Milica <djoric.milica91@yahoo.com>
2021-06-01 16:56:14 -0400

    day 7 iframe...

--- src/test/java/com/cybertek/tests/day7_alerts_iframes_windows/iFramePractice.java
+++ src/test/java/com/cybertek/tests/day7_alerts_iframes_windows/iFramePractice.java
@@ -1,7 +1,9 @@
 package com.cybertek.tests.day7_alerts_iframes_windows;
 
 import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -22,6 +24,17 @@ public class iFramePractice {
     }
     @Test
     public void iframe_test(){
+        //#1- changing to iframe by index
+        driver.switchTo().frame(0);
+
+        //#2- LOCATE as a Web Element to swich
+        //We find and locate <iframe> web element from the page
+        WebElement iframe = driver.findElement(By.xpath("//iframe[@class='tox-edit-area-iframe']"));
+
+        //locating web element text
+        WebElement yourContentGoesHereText = driver.findElement(By.xpath("//p"));
+
+        //#4 Assert: "Your content goes here" text is displayed.
 
     }
 }

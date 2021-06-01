0a8a59ee165fc7ed0cef3cdb8d315c57589486e9
Milica <djoric.milica91@yahoo.com>
2021-06-01 16:44:00 -0400

    day 7 iframe

--- src/test/java/com/cybertek/tests/day7_alerts_iframes_windows/iFramePractice.java
+++ src/test/java/com/cybertek/tests/day7_alerts_iframes_windows/iFramePractice.java
@@ -1,4 +1,27 @@
 package com.cybertek.tests.day7_alerts_iframes_windows;
 
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 public class iFramePractice {
+    WebDriver driver;
+    /*
+    1.Open Chrom browser
+    2 go to http.....
+     */
+    @BeforeClass
+    public void setupClass() {
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.get("http://practice.cybertekschool.com/iframe");
+    }
+    @Test
+    public void iframe_test(){
+
+    }
 }

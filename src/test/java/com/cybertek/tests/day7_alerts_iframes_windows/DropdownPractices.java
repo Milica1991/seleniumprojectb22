a1489605af01dc5fc37070af2be3c77f2f8711d7
Milica <djoric.milica91@yahoo.com>
2021-06-01 10:44:11 -0400

    day 7 - dropdown task 2

--- src/test/java/com/cybertek/tests/day7_alerts_iframes_windows/DropdownPractices.java
+++ src/test/java/com/cybertek/tests/day7_alerts_iframes_windows/DropdownPractices.java
@@ -1,17 +1,34 @@
 package com.cybertek.tests.day7_alerts_iframes_windows;
 
 import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
 public class DropdownPractices {
+    WebDriver driver;
+    /*
+    1.Open Chrom browser
+    2 go to http.....
+     */
     @BeforeClass
     public void setupClass(){
-        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.get("http://practice.cybertekschool.com/dropdown");
+
+    }
+
+    @Test
+    public void tc1_simple_dropdown_test(){
+        //3. Verify "Simple dropdown" defult selected value is correct
+        //Expected:' Please selected an option"
+        Select simpleDropdown = new Select(driver.findElement(By.xpath("//select[@id='dropdown']")));
 
     }
 }

694e1c86497ce1d4e718d71efe6d2cd4ddf6cd40
Milica <djoric.milica91@yahoo.com>
2021-06-17 09:02:22 -0400

    day 11 - driver

---
+++ src/test/java/com/cybertek/utilities/Driver.java
@@ -0,0 +1,8 @@
+package com.cybertek.utilities;
+
+public class Driver {
+    /**
+     * Creating the private constructor so this class object is not reachable from outside
+     */
+    private Driver(){}
+}

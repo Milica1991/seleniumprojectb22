022e37913226f0333e61a0ecff1b05d842ba6227
Milica <djoric.milica91@yahoo.com>
2021-06-17 07:51:26 -0400

    day 11 - tastbase

---
+++ src/test/java/com/cybertek/tests/day10_webtale_properties_practices/Table_Tasks.java
@@ -0,0 +1,22 @@
+package com.cybertek.tests.day10_webtale_properties_practices;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Table_Tasks {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setuoMethod(){
+
+    }
+
+
+    @Test
+    public void task3_return_times_due_amount(){
+
+
+    }
+}

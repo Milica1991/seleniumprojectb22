930f8f3f8c8d08788c8b50ba014a34e3a466c869
Milica <djoric.milica91@yahoo.com>
2021-06-17 09:06:14 -0400

    day 11 - driver

--- src/test/java/com/cybertek/utilities/Driver.java
+++ src/test/java/com/cybertek/utilities/Driver.java
@@ -1,8 +1,16 @@
 package com.cybertek.utilities;
 
+import org.openqa.selenium.WebDriver;
+
 public class Driver {
     /**
      * Creating the private constructor so this class object is not reachable from outside
      */
     private Driver(){}
+
+    /**
+     * Making our driver instance so that it is not reachable from outside of the class
+     * we make static,because we want it to run before everything else,and we will use in a static method
+     */
+    private static WebDriver driver;
 }

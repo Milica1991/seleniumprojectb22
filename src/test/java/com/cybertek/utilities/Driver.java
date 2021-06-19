6e0247c4ffd7ceb08d489763101f45da0ad250ac
Milica <djoric.milica91@yahoo.com>
2021-06-19 17:59:09 -0400

    utilities

--- src/test/java/com/cybertek/utilities/Driver.java
+++ src/test/java/com/cybertek/utilities/Driver.java
@@ -8,32 +8,34 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import java.util.concurrent.TimeUnit;
 
 public class Driver {
-    /**
-     * Creating the private constructor so this class object is not reachable from outside
-     */
+    /*
+   Creating the private constructor so this class' object
+   is not reachable from outside
+    */
     private Driver(){}
 
-    /**
-     * Making our driver instance so that it is not reachable from outside of the class
-     * we make static,because we want it to run before everything else,and we will use in a static method
+    /*
+    Making our 'driver' instance private so that it is not reachable from outside of the class.
+    We make it static, because we want it to run before everything else, and also we will use it in a static method
      */
     private static WebDriver driver;
 
-    /**
-     * Creating re-usable method that will return same driver instance everytime we call it.
+    /*
+    Creating re-usable utility method that will return same 'driver' instance everytime we call it.
      */
     public static WebDriver getDriver(){
 
         if (driver == null){
 
-            /** We read our browser type from configuration.properties file using .getProperty method we
-             * creating in ConfigurationReader class
+            /*
+            We read our browser type from configuration.properties file using
+            .getProperty method we creating in ConfigurationReader class.
              */
             String browserType = ConfigurationReader.getProperty("browser");
 
-            /**
-             * Depending on the browser type our switch statement will determine to open specific type
-             * of browser/driver
+            /*
+            Depending on the browser type our switch statement will determine
+            to open specific type of browser/driver
              */
             switch (browserType){
                 case "chrome":
@@ -48,25 +50,27 @@ public class Driver {
                     driver.manage().window().maximize();
                     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
                     break;
-
             }
-
         }
-        /** same driver instance will be returned every time we call Driver.getDriver(); method
+
+        /*
+        Same driver instance will be returned every time we call Driver.getDriver(); method
          */
         return driver;
 
+
     }
 
-    /**
-     *This method make sure we have some form of driver sesion or driver id has
-     * Eather null or not null it must exist
+    /*
+    This method makes sure we have some form of driver sesion or driver id has.
+    Either null or not null it must exist.
      */
     public static void closeDriver(){
-        if (driver!=null){
+        if(driver!=null){
             driver.quit();
             driver=null;
         }
-
     }
+
+
 }
